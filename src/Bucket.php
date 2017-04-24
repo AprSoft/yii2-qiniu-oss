d57950b8d81aea35485eebf3c7220cb2070a061a
张欣宇 <176956856@qq.com>
2017-04-24 18:07:11 +0800

    修改文件目录结构。

---
+++ src/Bucket.php
@@ -0,0 +1,20 @@
+<?php
+namespace AprSoft\QiNiu\OSS;
+
+use Yii;
+use yii\base\Component;
+use yii\log\Logger;
+
+
+/**
+ * Class OssClient
+ *
+ * Object Storage Service(OSS) 的客户端类，封装了用户通过OSS API对OSS服务的各种操作，
+ * 用户通过OssClient实例可以进行 Bucket, ACL等操作，具体
+ * 的接口规则可以参考官方OSS API文档
+ */
+
+class Bucket extends Component
+{
+
+}
---
+++ src/Image.php
@@ -0,0 +1,11 @@
+<?php
+namespace AprSoft\QiNiu\OSS;
+
+use Yii;
+use yii\base\Component;
+use yii\log\Logger;
+
+class Image extends Component
+{
+    
+}
