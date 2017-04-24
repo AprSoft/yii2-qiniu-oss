8f5cdc642d9d23188e16116215e6c095c204c6c0
张欣宇 <176956856@qq.com>
2017-04-24 17:48:51 +0800

    初始化。

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace AprSoft\QiNiu\OSS;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
