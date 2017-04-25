427e43c3d2be0c6cf1ef37d3282e6a5f73b47b62
张欣宇 <176956856@qq.com>
2017-04-25 18:13:22 +0800

    添加部分bucket操作.

--- src/Bucket.php
+++ src/Bucket.php
@@ -1,20 +1,44 @@
 <?php
 namespace AprSoft\QiNiu\OSS;
 
+use Qiniu\Storage\BucketManager;
+use Qiniu\Auth;
 use Yii;
 use yii\base\Component;
 use yii\log\Logger;
 
 
-/**
- * Class OssClient
- *
- * Object Storage Service(OSS) 的客户端类，封装了用户通过OSS API对OSS服务的各种操作，
- * 用户通过OssClient实例可以进行 Bucket, ACL等操作，具体
- * 的接口规则可以参考官方OSS API文档
- */
-
 class Bucket extends Component
 {
 
+    public $accessKeyId;
+
+    public $accessKeySecret;
+
+    private $_manager;
+
+    public function getManager()
+    {
+        if($this->_manager === null){
+            $this->setManager(new Auth($this->accessKeyId, $this->accessKeySecret));
+        }
+        return $this->_manager;
+    }
+
+    public function setManager(Auth $auth)
+    {
+        $this->_manager = new BucketManager($auth);
+    }
+
+    public function list()
+    {
+        try {
+             $buckets = $this->manager->buckets();
+        } catch (OssException $e) {
+            Yii::getLogger()->log($e->getMessage(), Logger::LEVEL_ERROR, 'qiniu-oss');
+            return false;
+        }
+        return $buckets;
+
+    }
 }
